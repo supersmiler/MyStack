579b79b903d507f7d0888892a5b91731af4f7090
Niels <niels.evers@hotmail.com>
2020-06-17 05:54:01 -0700

    First setup of reverse linked list

---
+++ mystack_reverse_list/product/main.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#include "mystack.h"
+#include "linked_list.h"
+
+ELEMENT *list = NULL;
+
+void printLinkedList(ELEMENT *listToPrint)
+{
+
+}
+
+int reverseList(ELEMENT *list)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+    
+    printf("test123");
+    return 0;
+}
